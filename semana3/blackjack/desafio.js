d5c1f8bf27804eaedcae6a98385cf3f84d2f54cb
ArturCandelori <artur.candelori@hotmail.com>
2020-04-04 10:34:31 -0300

    Fiz o desafio

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -10,4 +10,66 @@
  * 
  * 
  */
+alert("Bem vindo ao jogo de Blackjack!");
 
+if (confirm("Quer iniciar uma nova rodada?")) {
+
+   let cartasUsuario = [comprarCarta(),comprarCarta()];
+   let pontuacaoUsuario = cartasUsuario[0].valor + cartasUsuario[1].valor;
+   
+   let cartasComputador = [comprarCarta(),comprarCarta()];
+   let pontuacaoComputador = cartasComputador[0].valor + cartasComputador[1].valor;
+   
+   //item 8//
+   while (cartasUsuario[0].valor === 11 && cartasUsuario[1].valor === 11) {
+      cartasUsuario = [comprarCarta(),comprarCarta()];
+      pontuacaoUsuario = cartasUsuario[0].valor + cartasUsuario[1].valor;
+   }
+   while (cartasComputador[0].valor === 11 && cartasComputador[1].valor === 11) {
+      cartasComputador = [comprarCarta(),comprarCarta()];
+      pontuacaoComputador = cartasComputador[0].valor + cartasComputador[1].valor;
+   }
+
+   //itens 9 e 10//
+   let mensagemUsuario = cartasUsuario[0].texto+" "+cartasUsuario[1].texto;              
+   while (confirm("Suas cartas são "+mensagemUsuario+". A carta revelada do computador é "+cartasComputador[0].texto+
+                  "\n"+
+                  "Deseja comprar mais uma carta?")) {
+
+      const carta = comprarCarta();
+      cartasUsuario.push(carta);
+      mensagemUsuario += " "+carta.texto;
+      pontuacaoUsuario += carta.valor;
+
+   }
+      //item 11//
+      let mensagemComputador = cartasComputador[0].texto +" "+ cartasComputador[1].texto;
+
+      while (pontuacaoComputador < pontuacaoUsuario) {
+         const carta = comprarCarta();
+         cartasComputador.push(carta);
+         mensagemComputador += " "+carta.texto;
+         pontuacaoComputador += carta.valor;
+      }
+
+      let mensagemFinal = "";
+      //item 12//
+      if(pontuacaoUsuario > 21) {
+         mensagemFinal = "O computador venceu!";
+      }
+      if(pontuacaoUsuario < 21 && pontuacaoComputador > 21) {
+         mensagemFinal = "O usuário venceu!";
+      }
+      if(pontuacaoComputador < 21 && pontuacaoComputador > pontuacaoUsuario) {
+         mensagemFinal = "O computador venceu!";
+      }
+      if(pontuacaoUsuario === pontuacaoComputador) {
+         mensagemFinal = "Empate!";
+      }
+      alert("Suas cartas são "+mensagemUsuario+". Sua pontuação é "+pontuacaoUsuario+".\n" +
+            "As cartas do computador são "+mensagemComputador+". A pontuação do computador é "+pontuacaoComputador+".\n" +
+            mensagemFinal);  
+      
+}  else {
+   alert("O jogo acabou.");
+}
