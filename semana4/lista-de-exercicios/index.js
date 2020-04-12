b7f9cd34568a51bdc78343a558dcb06afc4844fc
ArturCandelori <artur.candelori@hotmail.com>
2020-04-12 12:10:11 -0300

    Exercícios de leitura - 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,10 @@
+/*
+Exercícios de leitura de código
+
+1. 
+-A função conversorDeMoeda aceita como argumento valoEmDolar
+-No seu corpo cria uma variável cotação, que é inserido pelo usuário e convertido para Number
+-A funcão retorna uma string com o valor em dólares usado como argumento vezes o valor da cotação inserido pelo usuário
+-A variável meuDinheiro armazena o resultado da função conversorDeMoeda com valorEmDolar = 100
+-Se usarmos cotação = 5, teremos meuDinheiro = 500
+-Imprime no console o valor de meuDinheiro
