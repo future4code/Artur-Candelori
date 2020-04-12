49299db9cc8aba18adb571ed1f6cc1ff43b73057
ArturCandelori <artur.candelori@hotmail.com>
2020-04-12 12:22:13 -0300

    2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -8,3 +8,16 @@ Exercícios de leitura de código
 -A variável meuDinheiro armazena o resultado da função conversorDeMoeda com valorEmDolar = 100
 -Se usarmos cotação = 5, teremos meuDinheiro = 500
 -Imprime no console o valor de meuDinheiro
+
+2.
+-A função investeDinheiro recebe dois argumentos: tipoDeInvestimento e valor
+-tipoDeInvestimento é uma string, valor é number
+-Declara uma variável sem atribuir valor a ela (undefined)
+-Usa o switch para determinar o que fazer com o valor dependendo do tipoDeInvestimento
+-O produto do valor e o coeficiente é armazenado em valorAposInvestimento
+-Função retorna valorAposInvestimento
+-novoMontante = 150 * 1.1 = 165
+-segundoMontante recebe um tipoDeInvestimento que não é igual a nenhum case, portanto vai para o default
+-Imprime 165
+-Mostra o alerta "TIPO DE INVESTIMENTO INFORMADO INCORRETO!"
+  
