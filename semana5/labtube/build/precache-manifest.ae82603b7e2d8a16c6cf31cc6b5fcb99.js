553ee5fcf216754c67af8c98730649ed964ac1b9
ArturCandelori <artur.candelori@hotmail.com>
2020-04-14 17:15:17 -0300

    labedin

---
+++ semana5/labtube/build/precache-manifest.ae82603b7e2d8a16c6cf31cc6b5fcb99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4848384666f8cbcb5424103e482038c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb25a819228117b5c694",
+    "url": "/static/css/main.7073c9df.chunk.css"
+  },
+  {
+    "revision": "df3b32f12c1190c3881d",
+    "url": "/static/js/2.be65b813.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be65b813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb25a819228117b5c694",
+    "url": "/static/js/main.c4786eeb.chunk.js"
+  },
+  {
+    "revision": "99c77c7c3bf36e3d79a0",
+    "url": "/static/js/runtime-main.9cb942d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
