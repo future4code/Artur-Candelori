1fe8e11d2e6b80f8dd9e0a3905c5fb8353f0fd02
ArturCandelori <artur.candelori@hotmail.com>
2020-04-22 16:56:33 -0300

    lista de tarefas

---
+++ semana6/formulario/build/precache-manifest.ee7fda96c1a789961835145821313cc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a21318e982d7b7a8918462a5e8c2f93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6e55426fd7d9b580ade",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "57f0e223d42edb65bbe5",
+    "url": "/static/js/2.a4afc0a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4afc0a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e55426fd7d9b580ade",
+    "url": "/static/js/main.c480bc2f.chunk.js"
+  },
+  {
+    "revision": "fd54f4f24e88b4dd3435",
+    "url": "/static/js/runtime-main.522d8101.js"
+  }
+]);
