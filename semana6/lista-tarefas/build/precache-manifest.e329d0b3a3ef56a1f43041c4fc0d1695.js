661fb813b8b215766e59be1c1bc4a1d34cb9d0e9
ArturCandelori <artur.candelori@hotmail.com>
2020-04-27 16:46:17 -0300

    create playlist

---
+++ semana6/lista-tarefas/build/precache-manifest.e329d0b3a3ef56a1f43041c4fc0d1695.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c862c2deb9395c4486f93f9f6baa6ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9f13ecdee1a06b6c345",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "28e1f6e4c00be6f90c95",
+    "url": "/static/js/2.8fdc7c9c.chunk.js"
+  },
+  {
+    "revision": "b9f13ecdee1a06b6c345",
+    "url": "/static/js/main.81017a86.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
